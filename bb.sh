b0d600e1b75c7e9b1cfd1b130989104645ddd1c3
zmyth <Aaron1992@users.noreply.github.com>
2018-02-10 22:54:36 +0800

    Create bb.sh

---
+++ bb.sh
@@ -0,0 +1,4 @@
+yum install epel-release -y
+yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto c-ares-devel libev-devel libsodium-devel mbedtls-devel -y
+su -c 'yum update'
+su -c 'yum install shadowsocks-libev'
