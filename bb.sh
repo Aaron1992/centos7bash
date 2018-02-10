185ad73f7736546d77bb8842c56633aaa2238cbe
zmyth <Aaron1992@users.noreply.github.com>
2018-02-10 23:12:03 +0800

    Update bb.sh

--- bb.sh
+++ bb.sh
@@ -3,3 +3,7 @@ yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto
 su -c 'yum update'
 su -c 'yum install shadowsocks-libev'
 cp magic ~/config.json
+
+#cp damaon service to systemd
+
+systemctl daemon-reload
